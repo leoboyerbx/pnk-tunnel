d917968415b143f65e474f8a96d56b28ef94112a
leoboyerbx <leoboyer.bx@gmail.com>
2021-04-19 18:02:22 +0200

    WIP Client

--- client.sh
+++ client.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "PnkTunnel: Secure tunnel to localhost via LBXS"
+    echo "----------------------------------------------"
+    echo "          This is the client script           "
+    echo "----------------------------------------------"
+    echo "Usage: pnktunnel PORT SUBDOMAIN"
+    echo
+    exit
+fi
+
+ssh -p 8022 -t -R 9001:localhost:3000 leo@leoboyer.dev pnktunnel lefaucigny 9001
